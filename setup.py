eb06e1dd6018bf4ce900b3a3eff77fc11de7a2b2
Erick <eribuit9@gmail.com>
2025-12-29 17:59:55 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miRepoRelases",                          
+    version="0.1.0",                               
+    packages=find_packages(),                      
+    description="Un paquete pip simple de saludo", 
+    author="Erick Buitrago",                       
+    author_email="ericksbp23@gmail.com",           
+    url="https://github.com/aguapanela-a/repoRelase",    
+)
